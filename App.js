600d465e0a69cfdfeeb231d18dd37c4d19af1e41
OMRFRKYL <omeryolu16@gmail.com>
2022-11-22 19:15:06 +0300

    SearchBar added and proje end.

--- App.js
+++ App.js
@@ -2,23 +2,35 @@ import React from "react";
 import {View,Text, SafeAreaView,StyleSheet,FlatList} from "react-native"
 import data from "./src/data.json"
 import SongCard from "./src/components/SongCard"
+import SearchBar from "./src/components/SearchBar";
+import { useState } from "react";
 
 
 const App=()=>{
+  const handleSearch=(text)=>{
+    const filteredList = data.filter(song=>{
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
 
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+    setList(filteredList);
+
+  };
   const renderSong = ({item})=> <SongCard song={item}/>
   const renderSeperator = ()=> <View style={styles.seperator}/>
+  const [list, setList] = useState(data)
 
   return(
     <SafeAreaView style={styles.container}>
-    <View style={styles.container}>
+    <SearchBar handleSearch={handleSearch}/>
       <FlatList
       keyExtractor={item=> item.id}
-      data={data}
+      data={list}
       renderItem={renderSong}
       ItemSeparatorComponent={renderSeperator}
       />
-    </View>
+  
     </SafeAreaView>
   )
 }
